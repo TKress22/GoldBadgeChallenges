c96e697ff280f008f24ef404e4ed823495ab1844
VividerAphid <trever.kress@gmail.com>
2019-02-20 16:41:29 -0500

    Updates for challenge_03

--- Challenge_03/OutingRepository.cs
+++ Challenge_03/OutingRepository.cs
@@ -8,6 +8,25 @@ namespace Challenge_03
 {
     public class OutingRepository
     {
-        
+        List<Outing> outings = new List<Outing>();
+
+        public void AddOuting(Outing add)
+        {
+            outings.Add(add);
+        }
+        public List<Outing> GetOutings()
+        {
+            return outings;
+        }
+        public double GetGrandTotal()
+        {
+            double total = 0;
+            return total;
+        }        
+        public double GetTotalByType(OutingType type)
+        {
+            double total = 0;
+            return total;
+        }
     }
 }
--- Challenge_03_Test/Challenge03Test.cs
+++ Challenge_03_Test/Challenge03Test.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Challenge_03_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class Challenge03Test
     {
         [TestMethod]
         public void TestMethod1()
