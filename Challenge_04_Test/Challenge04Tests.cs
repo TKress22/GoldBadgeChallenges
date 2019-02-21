f60e65c9704f3376f920ac6d0490221a81493c24
VividerAphid <trever.kress@gmail.com>
2019-02-21 13:16:05 -0500

    Finished unit testing challenge_04

--- Challenge_04_Test/Challenge04Tests.cs
+++ Challenge_04_Test/Challenge04Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Challenge_04;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Challenge_04_Test
@@ -7,8 +9,41 @@ namespace Challenge_04_Test
     public class Challenge04Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddBadgeTest()
         {
+            BadgeRepository bRepo = new BadgeRepository();
+            Dictionary<int, Badge> badges = bRepo.GetBadges();
+
+            Badge test = new Badge(123, new List<string> { "Ye", "Yoi", "Yo" });
+            bRepo.AddBadge(test);
+
+            Assert.IsTrue(badges.ContainsValue(test));
+        }
+
+        [TestMethod]
+        public void EditBadgeAddTest()
+        {
+            BadgeRepository bRepo = new BadgeRepository();
+            Dictionary<int, Badge> badges = bRepo.GetBadges();
+
+            Badge test = new Badge(123, new List<string> { "Ye", "Yoi", "Yo" });
+            bRepo.AddBadge(test);
+            bRepo.EditBadge(test.BadgeID, false, "F5");
+
+            Assert.IsTrue(badges[test.BadgeID].Doors.Contains("F5"));
+        }
+
+        [TestMethod]
+        public void EditBadgeRemoveTest()
+        {
+            BadgeRepository bRepo = new BadgeRepository();
+            Dictionary<int, Badge> badges = bRepo.GetBadges();
+
+            Badge test = new Badge(123, new List<string> { "Ye", "Yoi", "Yo" });
+            bRepo.AddBadge(test);
+            bRepo.EditBadge(test.BadgeID, true, "Ye");
+
+            Assert.IsFalse(badges[test.BadgeID].Doors.Contains("Ye"));
         }
     }
 }
